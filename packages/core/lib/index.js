478d723928c245c8fe129471b7b21f1c0dc53567
zhan <846184637@qq.com>
2021-08-30 15:58:45 +0800

    init

---
+++ packages/core/lib/index.js
@@ -0,0 +1,10 @@
+'use strict';
+console.log("3333");
+const utils = require("@hetao-cli/utils");
+module.exports = core;
+
+function core() {
+    // TODO
+    console.log("kdlfjsdkfls");
+    utils()
+}
